096f2a5766c8f2bc9eb312a109836ee4dc9abd84
xxswaganimexx <93254984+RenzyyyCursors@users.noreply.github.com>
2026-08-21 23:36:38 +0530

    Completely fixed the calculator and bugs

--- Calculator_Project.py
+++ Calculator_Project.py
@@ -1,6 +1,6 @@
 #%%
 tot = 0
-def calc(n1,n2,ops,tot):
+def calc(n1,n2,ops):
     if ops =='+':
         tot = n1+n2
     if ops =='-':
@@ -15,21 +15,34 @@ def calc(n1,n2,ops,tot):
         tot = n1%n2
     return tot
 
+clear = 'y'
 first = True 
 while True:
     userin = input("Enter to begin or (q) to quit")
+
     if userin.lower() == 'q':
         break
-    if 
-    clear = input("Continue with current answer?. (Y) for yes (N) for No")
+
+    if not first:
+        clear = input("Clear answer?. (Y) for yes (N) for No")
+        
     ops = input("Input Operation [+,-,*,/,^,%]")
+
     if clear.lower() == 'n':
         n3 = int(input("Next Number: "))
+        tot = calc(tot,n3,ops)
+        print(tot)
         
     elif clear.lower() == 'y':
         n1 = int(input("1st Number: "))
         n2 = int(input("2nd Number: "))
+        tot = 0
+        tot = calc(n1,n2,ops)
+        print(tot)
+    first = False
 
 
 
 
+
+# %%
