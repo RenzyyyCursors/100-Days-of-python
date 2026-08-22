22bd704037975cc86a577a71155fcc134d2f9ddb
xxswaganimexx <93254984+RenzyyyCursors@users.noreply.github.com>
2026-08-22 23:54:11 +0530

    all comments written

--- Calculator_Project.py
+++ Calculator_Project.py
@@ -1,6 +1,7 @@
 #%%
 tot = 0
-def calc(n1,n2,ops):
+def calc(n1,n2,ops): 
+# If else tree of all operations
     if ops =='+':
         tot = n1+n2
     if ops =='-':
@@ -20,19 +21,23 @@ first = True
 while True:
     userin = input("Enter to begin or (q) to quit")
 
+    # break statement
     if userin.lower() == 'q':
         break
 
+    # checking if first time (if so two arguments are passed)
     if not first:
         clear = input("Clear answer?. (Y) for yes (N) for No")
         
     ops = input("Input Operation [+,-,*,/,^,%]")
 
+    # one argument with prev addition
     if clear.lower() == 'n':
         n3 = int(input("Next Number: "))
         tot = calc(tot,n3,ops)
         print(tot)
-        
+
+    # two addition method   
     elif clear.lower() == 'y':
         n1 = int(input("1st Number: "))
         n2 = int(input("2nd Number: "))
