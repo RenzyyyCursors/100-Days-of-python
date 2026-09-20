4aeadbbb8b0b4ed2aafab9d69cb5acb9d0082e1a
xxswaganimexx <93254984+RenzyyyCursors@users.noreply.github.com>
2026-09-21 00:41:13 +0530

    Day 18 (started)

---
+++ 17.Quiz Game/18.Turtle/main.py
@@ -0,0 +1,19 @@
+from turtle import Turtle,Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.color("red")
+
+for i in range(4):
+    timmy_the_turtle.left(90)
+    timmy_the_turtle.forward(100)
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
