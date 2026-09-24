96753a767d9352048baa4c1f259ced71fe0e4d72
xxswaganimexx <93254984+RenzyyyCursors@users.noreply.github.com>
2026-09-24 21:15:34 +0530

    Update etch-a-sketch.py

--- Day 19.Higher Order/etch-a-sketch.py
+++ Day 19.Higher Order/etch-a-sketch.py
@@ -2,6 +2,7 @@ from turtle import Turtle,Screen
 
 t = Turtle()
 s = Screen()
+t.speed(0)
 
 def f():
     t.forward(10)
@@ -10,10 +11,10 @@ def b():
     t.backward(10)
 
 def r():
-    t.right(10)
+    t.right(15)
 
 def l():
-    t.left(10)
+    t.left(15)
 
 def up():
     t.penup()
@@ -21,12 +22,23 @@ def up():
 def dp():
     t.pendown()
 
+def home():
+    t.penup()
+    t.home()
+    t.pendown()
+
+def clc():
+    t.clear()
+    home()
+
 s.onkeypress(f,'Up')
 s.onkeypress(b,'Down')
 s.onkeypress(r,'Right')
 s.onkeypress(l,'Left')
 s.onkeypress(up,'x')
 s.onkeypress(dp,'c')
+s.onkeypress(home,'h')
+s.onkeypress(clc,'f')
 
 
 
